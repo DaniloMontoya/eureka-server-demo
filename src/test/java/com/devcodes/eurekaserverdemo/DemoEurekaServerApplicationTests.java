fb0134a84d720557c26726f1c1d0b3bef5ef49dc
DaniloMontoya <danilo9831montoya@gmail.com>
2023-03-10 10:06:19 -0500

    -Initial commit for eureka server

---
+++ src/test/java/com/devcodes/eurekaserverdemo/DemoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcodes.eurekaserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
